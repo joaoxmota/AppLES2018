47f31f8d182befb4dda722b0172a8836b9450ae6
Flávio <flavio.diminuto@outlook.com>
2018-10-01 22:59:32 -0300

    Implementando Login
    Adicionado a activitie Home para teste de Login

--- app/src/main/java/ipro/les2018/Login.java
+++ app/src/main/java/ipro/les2018/Login.java
@@ -5,24 +5,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONObject;
-import org.w3c.dom.Text;
+
+import java.lang.reflect.Type;
+
+import ipro.les2018.Model.Usuario;
 
 public class Login extends AppCompatActivity {
     private EditText edtLogin,edtSenha;
     private TextView btnEntar,btnCadastrar, btnRecuperarSenha;
-    String login,senha;
+    private String login,senha,json;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +49,7 @@ public class Login extends AppCompatActivity {
             btnEntar.setOnClickListener ( new View.OnClickListener () {
                 @Override
                 public void onClick(View v) {
-                    realizarLogin ();
+                    usuarioFromJson ();
                 }
             } );
         //Botão Cadastrar
@@ -64,28 +71,55 @@ public class Login extends AppCompatActivity {
         } );
 
     }
-    public void realizarLogin(){
+    public void usuarioFromJson(){
         this.login = edtLogin.getText ().toString ();
         this.senha = edtSenha.getText ().toString ();
+
+
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         String url = "http://192.168.15.4:8080/WebServ/webresources/generic/usuario/retornar/"+this.login+"/"+this.senha;
         JsonObjectRequest objectRequest = new JsonObjectRequest (
                 Request.Method.GET, url, null, new Response.Listener<JSONObject> () {
-            @Override
-            public void onResponse(JSONObject response) {
-                Log.e("Rest response", response.toString ());
-                System.out.print(response.toString ());
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.e("Rest response", response.toString ());
+                    setJson ( response.toString ());
+                    validarLogin ();
+                }
+            }, new Response.ErrorListener () {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Rest error", error.toString ());
+                    setJson ( error.toString ());
+
+                }
             }
-        }, new Response.ErrorListener () {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Rest response", error.toString ());
-                System.out.print(error.toString ());
-            }
-        }
         );
 
+
         requestQueue.add ( objectRequest );
+
+
+
+    }
+    public void setJson(String string){
+        this.json = string;
+    }
+    public void sendMessage(String texto){
+        Toast.makeText ( this, texto, Toast.LENGTH_SHORT ).show ();
+    }
+
+    public boolean validarLogin(){
+        Usuario us;
+        Gson g = new Gson ();
+        us = g.fromJson ( this.json, Usuario.class );
+        if(!(us.getEmail ().equals ( "noemail@nodomain.com" ))){
+            Intent it = new Intent ( this,Home.class );
+            startActivity ( it );
+        }else{
+            sendMessage ( "Usuario inválido" );
+        }
+        return true;
     }
 }
 //PENDÊNCIA: Implementar o botão Entrar (Realização do Login)
