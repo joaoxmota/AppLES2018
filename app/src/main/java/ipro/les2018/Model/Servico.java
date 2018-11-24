4d87666b2a93c6c95dee3325b4188722c8e2796c
joaoxmota <42226187+joaoxmota@users.noreply.github.com>
2018-11-24 15:59:26 -0200

    Add files via upload

---
+++ app/src/main/java/ipro/les2018/Model/Servico.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JM
+ */
+public class Servico {
+    private int id;
+    private String nome;
+    private Especialidade especialidade;
+    private float valor;
+    private double duracao;
+    
+    public Servico(String nome, Especialidade especialidade, float valor, double duracao)
+    {
+    this.nome = nome;
+    this.especialidade = especialidade;
+    this.valor = valor;
+    this.duracao = duracao;
+     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(Especialidade especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public double getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(double duracao) {
+        this.duracao = duracao;
+    }
+    
+    
+}
